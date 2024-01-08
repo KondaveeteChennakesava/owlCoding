31f08acc6644f33e7ce278c38bb8e9825971d1fe
Chennak2248 <21A91A04L6@aec.edu.in>
2024-01-08 23:31:03 +0530

    def minPairSum(self, nums: List[int]) -> int:

---
+++ practice/1877. Minimize Maximum Pair Sum in Array.py
@@ -0,0 +1,10 @@
+from typing import List
+def minPairSum(nums: List[int]) -> int:
+    nums.sort()
+    a = nums[:len(nums)//2]
+    b = nums[len(nums)//2:][::-1]
+    l = []
+    for i in range(len(nums)//2):
+        l.append(a[i]+b[i])
+    return max(l)
+print(minPairSum([3,5,4,2,4,6]))

39a16e6ff65247664571e24a44a050d214849d70
Chennak2248 <21A91A04L6@aec.edu.in>
2024-01-10 23:39:34 +0530

    Bubble Sort in O(N)

--- sorting/bubbleSort.py
+++ sorting/bubbleSort.py
@@ -1,9 +1,13 @@
 def bubbleSort(arr):
     n = len(arr)
+    didSwap = 0
     for i in range(n-1,-1,-1):
         for j in range(i):
             if arr[j] > arr[j+1]:
                 arr[j+1],arr[j] = arr[j],arr[j+1]
+                didSwap = 1
+        if didSwap == 0:
+            break
 
 arr = list(map(int,input().split()))
 bubbleSort(arr)

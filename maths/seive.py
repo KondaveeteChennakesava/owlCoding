825dc0920cbaccd20db6e8e8db0b681fa649b971
Chennak2248 <21A91A04L6@aec.edu.in>
2024-01-21 15:47:54 +0530

    Optimized

--- maths/seive.py
+++ maths/seive.py
@@ -1,10 +1,11 @@
 def seive(n):
     primes = [i for i in range(n+1)]
     primes[0],primes[1] = 0,0
-    for i in range(2,n+1):
-        if primes[i] == i:
-            for j in range(i*i,n,i):
+    for i in range(2,int(n**0.5)+1):
+        if primes[i]:
+            for j in range(i*i,n+1,i):
                 primes[j] = i
     return primes
 n = 25
 factors = seive(n)
+print(factors)

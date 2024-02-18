f1fc9ce91d010ce09ecdfa24c0e8cf5fab40d847
Chennakesava K <96309319+KondaveeteChennakesava@users.noreply.github.com>
2024-02-18 16:45:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of leaf nodes in BST/sum-of-leaf-nodes-in-bst.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+##Structure of node
+'''
+class Node:
+    def __init__(self, data=0):
+        self.data=0
+        self.left=None
+        self.right=None
+'''
+
+class Solution:
+    def sumOfLeafNodes(self, root):
+        #Your code here
+        if root == None:
+            return 0
+        if root.left == None and root.right == None:
+            return root.data
+        return self.sumOfLeafNodes(root.left) + self.sumOfLeafNodes(root.right)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, data=0):
+        self.data=data
+        self.left=None
+        self.right=None
+
+
+def createNewNode(value):
+    temp=Node()
+    temp.data=value
+    temp.left=None
+    temp.right=None
+    return temp
+    
+
+def newNode(root,data):
+    if(root is None):
+        root=createNewNode(data)
+    elif(data<root.data):
+        root.left=newNode(root.left,data);
+    else:
+        root.right=newNode(root.right,data)
+        
+    return root
+
+for _ in range(int(input())):
+    root=None
+    sizeOfArray=int(input())
+    arr=[int(x) for x in input().strip().split()]
+    for i in arr:
+        root=newNode(root,i)
+    ob = Solution()
+    print(ob.sumOfLeafNodes(root))
+# } Driver Code Ends
